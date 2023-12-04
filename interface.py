3ab10399decbf33deeb65ac16871ba54c7bef930
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2023-12-04 16:14:26 +0100

    Working gradio interface

--- interface.py
+++ interface.py
@@ -1,1 +1,23 @@
-# this file can and should be deleted
+import torch
+import requests
+from PIL import Image
+from torchvision import transforms
+
+model = torch.load('/home/anton/repos/lumos-mri/model.pkl')
+model.eval()
+
+labels = ["disease", "no_disease"]
+
+
+def predict(inp):
+  inp = transforms.ToTensor()(inp).unsqueeze(0)
+  with torch.no_grad():
+    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
+    confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
+  return confidences
+
+import gradio as gr
+
+gr.Interface(fn=predict,
+             inputs=gr.Image(type="pil"),
+             outputs=gr.Label(num_top_classes=3)).launch()
