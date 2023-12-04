12e02251795405f784d67be11f024fcb747d074a
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2023-12-04 16:24:08 +0100

    Fix image processing

--- interface.py
+++ interface.py
@@ -1,23 +1,37 @@
 import torch
-import requests
-from PIL import Image
+import torchvision.models as models
 from torchvision import transforms
+from PIL import Image
+import gradio as gr
 
+# Load the model and set to evaluation mode
 model = torch.load('/home/anton/repos/lumos-mri/model.pkl')
+model = model.to('cpu')  # Move the model to CPU
 model.eval()
 
 labels = ["disease", "no_disease"]
 
+def predict(img):
+    # Transform the image to the format expected by your model
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ])
 
-def predict(inp):
-  inp = transforms.ToTensor()(inp).unsqueeze(0)
-  with torch.no_grad():
-    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
-    confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
-  return confidences
+    # Process the image and predict
+    img = transform(img).unsqueeze(0)  # Add batch dimension
+    with torch.no_grad():
+        outputs = model(img)
+        _, predicted = torch.max(outputs, 1)
+        pred_label = labels[predicted.item()]
+        return pred_label
 
-import gradio as gr
+# Create Gradio interface
+iface = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),  # Ensure the input is a PIL image
+    outputs=gr.Label(num_top_classes=2)
+)
 
-gr.Interface(fn=predict,
-             inputs=gr.Image(type="pil"),
-             outputs=gr.Label(num_top_classes=3)).launch()
+iface.launch(share=True)
